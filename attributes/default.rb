bfd8982b0c58e9581d39e5bf33415e83e4062839
Brian Holtkamp <brholtkamp@gmail.com>
2016-07-30 21:45:51 -0500

    Removed man package

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['workstation']['home'] = ENV['HOME']
 default['workstation']['name'] = 'Brian Holtkamp'
 default['workstation']['email'] = 'brholtkamp@gmail.com'
 
-default['workstation']['packages'] = %w(git man tig vim)
+default['workstation']['packages'] = %w(git tig vim)
 default['workstation']['dotfiles'] = %w(editorconfig vimrc zshrc)
 default['workstation']['applications'] = []
 
