5406ad4f97c4b9e30a8591f9bf7fe06af508799d
Brian Holtkamp <brholtkamp@gmail.com>
2016-08-18 15:27:40 -0500

    Added in ownership line to dotfiles

--- recipes/install_dotfiles.rb
+++ recipes/install_dotfiles.rb
@@ -6,10 +6,12 @@
 node['workstation']['dotfiles'].each do |file|
   cookbook_file "#{node['workstation']['home']}/.#{file}" do
     source file
+    owner node['workstation']['user']
   end
 end
 
 template "#{node['workstation']['home']}/.gitconfig" do
   source 'gitconfig.erb'
   variables name: node['workstation']['name'], email: node['workstation']['email']
+  owner node['workstation']['user']
 end
