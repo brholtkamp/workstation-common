8561e93b7ec5b75fc8bdefb52e7b51eed190a664
Brian Holtkamp <brholtkamp@gmail.com>
2017-03-31 18:43:14 -0500

    Got rid of GNU/BSD incompatible color flag

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'brholtkamp@gmail.com'
 license 'MIT'
 description 'Installs all of the OS agnostic files and settings'
 long_description 'Handles all OS agnostic files and settings so all nodes are consistent'
-version '0.1.0'
+version '0.1.1'
 source_url 'https://github.com/brholtkamp/workstation-common'
 issues_url 'https://github.com/brholtkamp/workstation-common/issues'
 
